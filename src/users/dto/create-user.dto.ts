3764b94ee24fd01e4443b519f942b5f335f43946
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-18 13:01:16 -0300

    create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'É necessário informar o e-mail.' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'É necessário informar a senha.' })
+  password: string;
+}
